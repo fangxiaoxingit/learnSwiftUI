afae38f126bdfe5a447ed95d0c84849a9a69e171
fangxiaoxin <siyu.fang@360gst.com>
2022-05-23 23:59:14 +0800

    同步代码

---
+++ learnSwiftUI/learnSwiftUI/learnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnSwiftUIApp.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/23.
+//
+
+import SwiftUI
+
+@main
+struct learnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
