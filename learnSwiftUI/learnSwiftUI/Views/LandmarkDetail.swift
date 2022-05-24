18c98b5a66a8aa470d114f1eff44f24d092b3cd3
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-24 16:33:09 +0800

    更新第一章

--- learnSwiftUI/learnSwiftUI/Views/LandmarkDetail.swift
+++ learnSwiftUI/learnSwiftUI/Views/LandmarkDetail.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges:.top)
+                .frame(height: 200)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom,-130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(Color.gray)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                    .padding(.bottom, 5.0)
+                Text(landmark.description)
+            
+            }.padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
