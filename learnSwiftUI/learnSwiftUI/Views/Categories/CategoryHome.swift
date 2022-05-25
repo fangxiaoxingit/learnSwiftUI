af618af3eb39be86bdd82985bedd334437c6630c
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-25 20:44:54 +0800

    同步

---
+++ learnSwiftUI/learnSwiftUI/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
---
+++ learnSwiftUI/learnSwiftUI/Views/Categories/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/25.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
