49aff7af3632dba386977c5f4bace2946306d987
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-27 17:49:41 +0800

    同步

--- learnSwiftUI/learnSwiftUI/Views/PageView/PageViewController.swift
+++ learnSwiftUI/learnSwiftUI/Views/PageView/PageViewController.swift
@@ -5,4 +5,23 @@
 //  Created by 方小新 on 2022/5/27.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+
+}
