7ab17f102cebdac74491702947c711316d076e03
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-27 17:49:39 +0800

    同步

---
+++ learnSwiftUI/learnSwiftUI/Views/PageView/PageView.swift
@@ -0,0 +1,20 @@
+//
+//  PageView.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/27.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
---
+++ learnSwiftUI/learnSwiftUI/Views/PageView/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/27.
+//
+
+import Foundation
