ae64b156b2c3cd3e7f6b2768ea70a7695f0902ce
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-28 14:37:02 +0800

    同步

---
+++ learnSwiftUI/learnSwiftUI/Views/PageView/PageControl.swift
@@ -0,0 +1,20 @@
+//
+//  PageControl.swift
+//  learnSwiftUI
+//
+//  Created by 方小新 on 2022/5/28.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
