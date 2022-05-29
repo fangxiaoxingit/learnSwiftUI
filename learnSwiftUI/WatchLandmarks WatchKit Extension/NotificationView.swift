f738d2259f38ca0b4e67f5d2d071abfd87dc0517
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-29 16:45:12 +0800

    同步代码

---
+++ learnSwiftUI/WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by 方小新 on 2022/5/29.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
