81eb0caafe0c94223afeb89d1eb370b7c6fb636d
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-30 14:18:37 +0800

    同步代码

---
+++ learnSwiftUI/WatchLandmarks WatchKit Extension/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by 方小新 on 2022/5/30.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
